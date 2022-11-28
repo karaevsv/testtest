46174bd964759c93f98e42d067e8247b304389c3
User <1@1.try>
2022-11-28 16:38:28 +0300

    DB work

--- main.py
+++ main.py
@@ -1,44 +1,79 @@
 import sys
+from PyQt5.QtSql import QSqlDatabase, QSqlTableModel
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QWidget, QAction, QTabWidget, QVBoxLayout, QTableView
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import pyqtSlot
 
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
-from PyQt5.QtWidgets import QMainWindow, QLabel
+
+class App(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.left = 0
+        self.top = 0
+        self.width = 300
+        self.height = 200
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+
+        self.table_widget = MyTableWidget(self)
+        self.setCentralWidget(self.table_widget)
+
+        self.show()
 
 
-class FirstForm(QMainWindow):
+class MyTableWidget(QWidget):
+    def __init__(self, parent):
+        super(QWidget, self).__init__(parent)
+        self.layout = QVBoxLayout(self)
+
+        # Initialize tab screen
+        self.tabs = QTabWidget()
+        self.tab1 = StaffWidget()
+        self.tab2 = QWidget()
+        self.tabs.resize(300, 200)
+
+        # Add tabs
+        self.tabs.addTab(self.tab1, "Staff")
+        self.tabs.addTab(self.tab2, "Tab 2")
+
+        # Add tabs to widget
+        self.layout.addWidget(self.tabs)
+        self.setLayout(self.layout)
+
+
+class StaffWidget(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
 
     def initUI(self):
-        self.setGeometry(300, 300, 300, 300)
-        self.setWindowTitle('Главная форма')
+        db = QSqlDatabase.addDatabase('QSQLITE')
+        # Укажем имя базы данных
+        db.setDatabaseName('123.db')
+        # И откроем подключение
+        db.open()
 
-        self.btn = QPushButton('Другая форма', self)
-        self.btn.setText('123')
-        self.btn.resize(self.btn.sizeHint())
-        self.btn.move(200, 400)
+        view = QTableView(self)
+        # Создадим объект QSqlTableModel,
+        # зададим таблицу, с которой он будет работать,
+        #  и выберем все данные
+        model = QSqlTableModel(self, db)
+        model.setTable('Hotel')
+        model.select()
 
-        self.btn.clicked.connect(self.open_second_form)
-
-    def open_second_form(self):
-        self.second_form = SecondForm(self, "Данные для второй формы")
-        self.second_form.show()
-
-
-class SecondForm(QWidget):
-    def __init__(self, *args):
-        super().__init__()
-        self.initUI(args)
-
-    def initUI(self, args):
-        self.setGeometry(300, 300, 300, 300)
-        self.setWindowTitle('Вторая форма')
-        self.lbl = QLabel(args[-1], self)
-        self.lbl.adjustSize()
+        # Для отображения данных на виджете
+        # свяжем его и нашу модель данных
+        view.setModel(model)
+        view.move(10, 10)
+        view.resize(617, 315)
+        self.layout = QVBoxLayout(self)
+        self.pushButton1 = QPushButton("Add")
+        self.layout.addWidget(view)
+        self.layout.addWidget(self.pushButton1)
+        self.setLayout(self.layout)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = FirstForm()
-    ex.show()
-    sys.exit(app.exec())
+    ex = App()
+    sys.exit(app.exec_())
